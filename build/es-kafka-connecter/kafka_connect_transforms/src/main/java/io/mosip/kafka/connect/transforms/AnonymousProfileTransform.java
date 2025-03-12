712a428f88a28fca90a626692a95449733f25a17
Venkatesh-Banoth-07 <venkatesh.banoth@technoforte.co.in>
2025-03-12 14:40:53 +0530

    updated key

--- build/es-kafka-connecter/kafka_connect_transforms/src/main/java/io/mosip/kafka/connect/transforms/AnonymousProfileTransform.java
+++ build/es-kafka-connecter/kafka_connect_transforms/src/main/java/io/mosip/kafka/connect/transforms/AnonymousProfileTransform.java
@@ -294,7 +294,7 @@ public abstract class AnonymousProfileTransform<R extends ConnectRecord<R>> impl
                      if (dist_name != null && districtMap.containsKey(dist_name)) {
                      updatedValue.put("district", districtMap.get(dist_name));
                      }
-                     String Tribe_name = (String) updatedValue.get("Tribe");
+                     String Tribe_name = (String) updatedValue.get("tribe");
                      if (Tribe_name != null && TribeMap.containsKey(Tribe_name)) {
                      updatedValue.put("tribe", TribeMap.get(Tribe_name));
                      }
